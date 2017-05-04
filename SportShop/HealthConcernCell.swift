c1ef2da50a529e24065617e9125677eb73c3e244
tejasranade <tejas.ranade@gmail.com>
2017-05-04 10:01:14 -0400

    updates to healthconcerns

---
+++ SportShop/HealthConcernCell.swift
@@ -0,0 +1,9 @@
+//
+//  HealthConcernCell.swift
+//  KinveyHealth
+//
+//  Created by Tejas on 5/3/17.
+//  Copyright © 2017 Kinvey. All rights reserved.
+//
+
+import Foundation
---
+++ SportShop/HealthConcernsController.swift
@@ -0,0 +1,9 @@
+//
+//  HealthConcernsController.swift
+//  KinveyHealth
+//
+//  Created by Tejas on 5/3/17.
+//  Copyright © 2017 Kinvey. All rights reserved.
+//
+
+import Foundation
