8e5187941f41662e23072147121a3f6594fc4e3e
Victor Hugo <hey.zooi@gmail.com>
2017-05-05 16:40:34 -0700

    Clinical Summary

--- SportShop/ClinicalSummaryController.swift
+++ SportShop/ClinicalSummaryController.swift
@@ -9,11 +9,12 @@
 import Foundation
 import UIKit
 import Kinvey
+import PromiseKit
 
 class ClinicalSummaryController: UITableViewController {
     
-    lazy var prescriptionDataStore = DataStore<Prescription>.collection()
-    lazy var immunizationDataStore = DataStore<Immunization>.collection()
+    lazy var prescriptionDataStore = DataStore<Prescription>.collection(.network)
+    lazy var immunizationDataStore = DataStore<Immunization>.collection(.network)
     
     enum Section: Int {
         case prescription = 0
@@ -39,32 +40,42 @@ class ClinicalSummaryController: UITableViewController {
     }
     
     func refresh() {
+        refreshControl!.beginRefreshing()
         refresh(refreshControl!)
     }
     
     @IBAction func refresh(_ sender: UIRefreshControl) {
-        loadPrescriptions()
-        loadImmunizations()
-    }
-    
-    func loadPrescriptions() {
-        prescriptionDataStore.find { (result: Result<[Prescription], Swift.Error>) in
-            switch result {
-            case .success(let prescriptions):
-                self.prescriptions = prescriptions
-            case .failure(_):
-                break
+        when(resolved: [loadPrescriptions(), loadImmunizations()]).always {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                sender.endRefreshing()
             }
         }
     }
     
-    func loadImmunizations() {
-        immunizationDataStore.find { (result: Result<[Immunization], Swift.Error>) in
-            switch result {
-            case .success(let immunizations):
-                self.immunizations = immunizations
-            case .failure(_):
-                break
+    func loadPrescriptions() -> Promise<Void> {
+        return Promise<Void> { fulfill, reject in
+            prescriptionDataStore.find { (result: Kinvey.Result<[Prescription], Swift.Error>) in
+                switch result {
+                case .success(let prescriptions):
+                    self.prescriptions = prescriptions
+                case .failure(_):
+                    break
+                }
+                fulfill()
+            }
+        }
+    }
+    
+    func loadImmunizations() -> Promise<Void> {
+        return Promise<Void> { fulfill, reject in
+            immunizationDataStore.find { (result: Kinvey.Result<[Immunization], Swift.Error>) in
+                switch result {
+                case .success(let immunizations):
+                    self.immunizations = immunizations
+                case .failure(_):
+                    break
+                }
+                fulfill()
             }
         }
     }
@@ -74,38 +85,65 @@ class ClinicalSummaryController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        switch section {
-        case Section.prescription.rawValue:
-            return prescriptions.count
-        case Section.immunization.rawValue:
-            return immunizations.count
-        default:
-            return 0
+        switch Section(rawValue: section)! {
+        case .prescription:
+            return max(1, prescriptions.count)
+        case .immunization:
+            return max(1, immunizations.count)
         }
     }
     
     override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 44
+        switch section {
+        case 0:
+            return 64 + 16
+        default:
+            return 64
+        }
     }
     
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        switch section {
-        case Section.prescription.rawValue:
-            let vc = UIStoryboard.viewController(identifier: "ClinicalSummaryHeaderViewController") as! ClinicalSummaryHeaderViewController
-            vc.label.text = "PRESCRIPTIONS"
-            return vc.view
-        case Section.immunization.rawValue:
-            let vc = UIStoryboard.viewController(identifier: "ClinicalSummaryHeaderViewController") as! ClinicalSummaryHeaderViewController
-            vc.label.text = "IMMUNIZATION"
-            return vc.view
-        default:
-            return nil
+        switch Section(rawValue: section)! {
+        case .prescription:
+            let header = UIStoryboard.viewController(identifier: "ClinicalSummaryHeaderViewController") as! ClinicalSummaryHeaderViewController
+            let view = header.view
+            header.label.text = "PRESCRIPTIONS"
+            return view
+        case .immunization:
+            let header = UIStoryboard.viewController(identifier: "ClinicalSummaryHeaderViewController") as! ClinicalSummaryHeaderViewController
+            let view = header.view
+            header.label.text = "IMMUNIZATION"
+            return view
         }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Immunization Cell")!
-        return cell
+        switch Section(rawValue: indexPath.section)! {
+        case .prescription:
+            if prescriptions.isEmpty {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "Empty Cell") as! ClinicalSummaryEmptyTableViewCell
+                cell.label.text = "No Prescriptions"
+                return cell
+            } else {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "Prescripton Cell") as! ClinicalSummaryTableViewCell
+                let prescription = prescriptions[indexPath.row]
+                cell.leftLabel.text = prescription.name
+                cell.rightLabel.text = "\(prescription.dose ?? 0) \(prescription.doseUnit ?? "")\n\(prescription.freqPeriod ?? "") \(prescription.freqUnit ?? "")"
+                return cell
+            }
+        case .immunization:
+            if immunizations.isEmpty {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "Empty Cell") as! ClinicalSummaryEmptyTableViewCell
+                cell.label.text = "No Immunizations"
+                return cell
+            } else {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "Immunization Cell") as! ClinicalSummaryTableViewCell
+                let immunization = immunizations[indexPath.row]
+                cell.leftLabel.text = immunization.name
+                cell.rightLabel.text = immunization.lastAdministered
+                return cell
+            }
+        }
     }
 
 }
@@ -114,5 +152,18 @@ class ClinicalSummaryHeaderViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var label: UILabel!
+    
+}
+
+class ClinicalSummaryEmptyTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    
+}
+
+class ClinicalSummaryTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var leftLabel: UILabel!
+    @IBOutlet weak var rightLabel: UILabel!
     
 }
